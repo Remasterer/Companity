4364e6944af135b0fffad62371a23259c5fe6f05
Remasterer <ronitrend@gmail.com>
2020-07-01 21:46:09 +0300

    finishe4d header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WhoWeAreComponent} from './pages/preview-pages/who-we-are/who-we-are.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'who-we-are', component: WhoWeAreComponent },
+  { path: '',   redirectTo: 'who-we-are', pathMatch: 'full' },
+  { path: '**', redirectTo: 'who-we-are' },
+  { path: '**', redirectTo: 'who-we-are'},  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

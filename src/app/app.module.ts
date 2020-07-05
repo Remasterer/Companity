f74c233576bf8abe15c0e94643b6093863d2c3c8
Remasterer <ronitrend@gmail.com>
2020-07-06 02:23:39 +0300

    all page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,38 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HeaderComponent} from './components/header/header.component';
 import { WhoWeAreComponent } from './pages/preview-pages/who-we-are/who-we-are.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
+
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WhoWeAreComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SwiperModule,
+    AnimateOnScrollModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/preview-pages/who-we-are/who-we-are.component.ts
+++ src/app/pages/preview-pages/who-we-are/who-we-are.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SwiperConfigInterface} from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-who-we-are',
@@ -8,8 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class WhoWeAreComponent implements OnInit {
 
   constructor() { }
-
+  index;
+  config: SwiperConfigInterface = {
+    a11y: true,
+    direction: 'horizontal',
+    slidesPerView: 1,
+    watchSlidesProgress: true,
+    navigation: true,
+    keyboard: true,
+    pagination: false,
+    centeredSlides: true,
+    loop: true,
+    roundLengths: true,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 100,
+    spaceBetween: 0,
+  };
   ngOnInit(): void {
   }
-
 }
